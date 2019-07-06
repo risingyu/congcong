432038c0326b213dad130afb39b6ef64b51e8407
risingyu <894199735@qq.com>
2019-07-06 21:22:46 +0800

    数据渲染

--- src/components/CiList/index.vue
+++ src/components/CiList/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="cinema_body">
         <ul>
-            <li>
+            <!-- <li>
                 <div>
                     <span>西安奥斯卡赛高国际影城</span>
                     <span class="q"><span class="price">25.8</span>元起</span>
@@ -55,6 +55,19 @@
                 <div class="card">
                     <div>小吃</div>
                     <div>影城卡</div>
+                </div>
+            </li> -->
+            <li v-for="item in cinemaList" :key="item.id">
+                <div>
+                    <span>{{ item.nm }}</span>
+                    <span class="q"><span class="price">{{ item.sellPrice }}</span>元起</span>
+                </div>
+                <div class="address">
+                    <span>{{ item.addr }}</span>
+                    <span>{{ item.distance }}</span>
+                </div>
+                <div class="card">
+                    <div v-for="(num,key) in item.tag" v-if="num===1" :key="key" :class="key | classCard">{{ key | formatCard }}</div>
                 </div>
             </li>
         </ul>
@@ -63,7 +76,50 @@
 
 <script>
 export default {
-    name : 'CiList'
+    name : 'CiList',
+    data(){
+        return {
+            cinemaList : []
+        };
+    },
+    mounted(){
+        this.axios.get('/api/cinemaList?cityId=10').then((res)=>{
+            var msg = res.data.msg;
+            if (msg === 'ok') {
+                this.cinemaList = res.data.data.cinemas;
+            }
+        });
+    },
+    filters : {
+        formatCard(key){
+            var card = [
+                { key : 'allowRefund', value : '改签' },
+                { key : 'endorse', value : '退' },
+                { key : 'sell', value : '折扣卡' },
+                { key : 'snack', value : '小吃' }
+            ];
+            for (var i = 0; i < card.length; i++) {
+                if (card[i].key === key) {
+                    return card[i].value;
+                }
+            }
+            return '';
+        },
+        classCard(key){
+            var card = [
+                { key : 'allowRefund', value : 'bl' },
+                { key : 'endorse', value : 'bl' },
+                { key : 'sell', value : 'or' },
+                { key : 'snack', value : 'or' }
+            ];
+            for (var i = 0; i < card.length; i++) {
+                if (card[i].key === key) {
+                    return card[i].value;
+                }
+            }
+            return '';
+        }
+    }
 }
 </script>
 
@@ -77,7 +133,7 @@ export default {
 .cinema_body .address{ font-size: 13px; color: #666;}
 .cinema_body .address span:nth-of-type(2){ float: right;}
 .cinema_body .card{ display: flex;}
-.cinema_body .card div{ padding: 0 3px; height: 15px; line-height: 15px; border-radius: 2px; color: #f90; border: 1px solid #f90;}
+.cinema_body .card div{ padding: 0 3px; height: 15px; line-height: 15px; border-radius: 2px; color: #f90; border: 1px solid #f90; margin: 2px;}
 .cinema_body .card div.or{ color: #f90; border: 1px solid #f90;}
 .cinema_body .card div.bl{ color: #589daf; border: 1px solid #589daf;}
 </style>
